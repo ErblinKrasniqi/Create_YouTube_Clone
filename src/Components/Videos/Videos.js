63789a8040a323005132e2c6dd939d63e6bd4a81
Erblin Krasniqi <erblini.kr@gmail.com>
2023-05-15 21:11:43 +0200

    Added more details and logic

--- src/Components/Videos/Videos.js
+++ src/Components/Videos/Videos.js
@@ -1,104 +1,67 @@
 import classes from "./Videos.module.css";
 
+const DUMMY_DATA = [
+  {
+    title: "Video Title in the west",
+    channel: "Channel Name",
+    views: "180k",
+    date: "1",
+    time: "year",
+    thumbNail: "/images/image1.jpg",
+    channelImage: "/images/profile1.jpg",
+    videoLength: "2:01",
+  },
+  {
+    title: "Just goingon a jureny on my way of finding the trueth",
+    channel: "Bixby",
+    views: "180k",
+    date: "1",
+    time: "year",
+    thumbNail: "/images/image2.jpg",
+    channelImage: "/images/profile2.jpg",
+    videoLength: "2:05",
+  },
+  {
+    title: "Video Title in the west",
+    channel: "Channel Name",
+    views: "180k",
+    date: "1",
+    time: "year",
+    thumbNail: "/images/image3.jpg",
+    channelImage: "/images/profile3.jpg",
+    videoLength: "2:01",
+  },
+];
+
 const Videos = () => {
   return (
     <div className={classes.container}>
-      <div className={classes.video}>
-        <div className={classes.image}>
-          <img alt="err" src="/image1.jpg"></img>
-          <h5>2:05</h5>
-        </div>
-        <div className={classes.videoInfo}>
-          <img alt="err" src="/image1.jpg"></img>
+      {DUMMY_DATA.map((data) => (
+        <div className={classes.video}>
+          <div className={classes.image}>
+            <img alt="err" src={data.thumbNail}></img>
+            <h5>{data.videoLength}</h5>
+          </div>
+          <div className={classes.videoInfo}>
+            <img alt="err" src={data.channelImage}></img>
 
-          <div className={classes.videoText}>
-            <h4>Video Title in the west</h4>
-            <div className={classes.verified}>
-              <h4>Channel Name</h4>
-              <img alt="err" src="/svgs/check.svg"></img>
+            <div className={classes.videoText}>
+              <div className={classes.dots}>
+                <h4>{data.title}</h4>
+
+                <img src="/svgs/dots.svg"></img>
+              </div>
+              <div className={classes.verified}>
+                <h4>{data.channel}</h4>
+                <img alt="err" src="/svgs/check.svg"></img>
+              </div>
+              <h2 className={classes.views}>
+                {data.views} views * {data.time} ago
+              </h2>
             </div>
-            <h4 className={classes.views}>180k views * 1 year ago</h4>
           </div>
         </div>
-      </div>
-      
-
-      <div className={classes.video}>
-        <div className={classes.image}>
-          <img alt="err" src="/image1.jpg"></img>
-          <h5>2:05</h5>
-        </div>
-        <div className={classes.videoInfo}>
-          <img alt="err" src="/image1.jpg"></img>
-
-          <div className={classes.videoText}>
-            <h4>Video Title in the west</h4>
-            <div className={classes.verified}>
-              <h4>Channel Name</h4>
-              <img alt="err" src="/svgs/check.svg"></img>
-            </div>
-            <h4 className={classes.views}>180k views * 1 year ago</h4>
-          </div>
-        </div>
-      </div>
-
-      <div className={classes.video}>
-        <div className={classes.image}>
-          <img alt="err" src="/image1.jpg"></img>
-          <h5>2:05</h5>
-        </div>
-        <div className={classes.videoInfo}>
-          <img alt="err" src="/image1.jpg"></img>
-
-          <div className={classes.videoText}>
-            <h4>Video Title in the west</h4>
-            <div className={classes.verified}>
-              <h4>Channel Name</h4>
-              <img alt="err" src="/svgs/check.svg"></img>
-            </div>
-            <h4 className={classes.views}>180k views * 1 year ago</h4>
-          </div>
-        </div>
-      </div>
-
-      <div className={classes.video}>
-        <div className={classes.image}>
-          <img alt="err" src="/image1.jpg"></img>
-          <h5>2:05</h5>
-        </div>
-        <div className={classes.videoInfo}>
-          <img alt="err" src="/image1.jpg"></img>
-
-          <div className={classes.videoText}>
-            <h4>Video Title in the west</h4>
-            <div className={classes.verified}>
-              <h4>Channel Name</h4>
-              <img alt="err" src="/svgs/check.svg"></img>
-            </div>
-            <h4 className={classes.views}>180k views * 1 year ago</h4>
-          </div>
-        </div>
-      </div>
-
-      <div className={classes.video}>
-        <div className={classes.image}>
-          <img alt="err" src="/image1.jpg"></img>
-          <h5>2:05</h5>
-        </div>
-        <div className={classes.videoInfo}>
-          <img alt="err" src="/image1.jpg"></img>
-
-          <div className={classes.videoText}>
-            <h4>Video Title in the west</h4>
-            <div className={classes.verified}>
-              <h4>Channel Name</h4>
-              <img alt="err" src="/svgs/check.svg"></img>
-            </div>
-            <h4 className={classes.views}>180k views * 1 year ago</h4>
-          </div>
-        </div>
-      </div>
-      
+      ))}
     </div>
   );
 };
